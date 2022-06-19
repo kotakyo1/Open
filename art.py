74567f2a44c6f3b8b74c8e6c1b06f1be0b7d430d
kotakyo1 <104491903+kotakyo1@users.noreply.github.com>
2022-06-19 16:42:06 +0600

    Update art.py

--- art.py
+++ art.py
@@ -1,8 +1,10 @@
 import pywhatkit
 
 def image():
-    image_path = input('введи путь к изображению ')
-    pywhatkit.image_to_ascii_art(img_path=image_path)
+    image_path = input('Введи путь к изображению ') #file path
+    image_path = image_path.replace('"', '')
+    file_name = input('Введи имя будущего для файла ') #future file's name
+    pywhatkit.image_to_ascii_art(img_path=image_path, output_file=file_name )
 
 def main():
     image()
