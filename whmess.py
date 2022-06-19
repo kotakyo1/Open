ce61e551420f84647ef08d8296dcd5f12c8ef9a1
kotakyo1 <104491903+kotakyo1@users.noreply.github.com>
2022-06-19 15:10:10 +0600

    Update whmess.py

--- whmess.py
+++ whmess.py
@@ -13,7 +13,7 @@ def send_message_inst():
     pywhatkit.sendwhatmsg_instantly(phone_no=number, message=message_text)
 
 def send_group():
-    name_group = 'Джентельмены удачи/W'
+    name_group = input('id группы ')
     message_text = input('что хочешь оправить ')
     thour = input('во сколько отвправить, введи время через двоеточие ').split(':')
     hour = int(thour[0])
@@ -21,8 +21,10 @@ def send_group():
     pywhatkit.sendwhatmsg_to_group(group_id=name_group, message=message_text, time_hour=hour, time_min=minute)
 
 def main():
-    send_message_inst()
-
+    pass
+    #send_message_inst()
+    #send_messages()
+    #send_group()
 
 if __name__ == '__main__':
     main()
